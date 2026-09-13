7b46035ae5decf9203aa5e68dffbd21dfbed0cbd
Orinbassar-Nuradil <nurdiloryrbasar@gmail.com>
2026-09-13 17:18:59 +0500

    Update HttpRequestDirector.java

--- HttpRequestDirector.java
+++ HttpRequestDirector.java
@@ -1,1 +1,13 @@
+public class HttpRequestDirector {
+    public void makeGetRequest(HttpRequestBuilder builder) {
+        builder.setMethod(HttpMethod.GET)
+               .setUrl("https://example.com");
+    }
+
+    public void makePostRequest(HttpRequestBuilder builder) {
+        builder.setMethod(HttpMethod.POST)
+               .setUrl("https://example.com")
+               .setBody("{\"name\": \"Ali\"}");
+    }
+}
 
