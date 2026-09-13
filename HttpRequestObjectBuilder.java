cef8875743040fd23ef5459fe02325fd738b7ac0
Orinbassar-Nuradil <nurdiloryrbasar@gmail.com>
2026-09-13 17:17:43 +0500

    Update HttpRequestObjectBuilder.java

--- HttpRequestObjectBuilder.java
+++ HttpRequestObjectBuilder.java
@@ -1,1 +1,34 @@
+public class HttpRequestObjectBuilder implements HttpRequestBuilder {
+    private String method;
+    private String url;
+    private String body;
+
+    @Override
+    public HttpRequestObjectBuilder setMethod(String method) {
+        this.method = method;
+        return this;
+    }
+
+    @Override
+    public HttpRequestObjectBuilder setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    @Override
+    public HttpRequestObjectBuilder setBody(String body) {
+        this.body = body;
+        return this;
+    }
+
+    public HttpRequest getResult() {
+        if (url == null || url.isBlank()) {
+            throw new IllegalStateException("URL is required");
+        }
+        if (HttpMethod.POST.equals(method) && (body == null || body.isBlank())) {
+            throw new IllegalStateException("POST requires a body");
+        }
+        return new HttpRequest(method, url, body);
+    }
+}
 
