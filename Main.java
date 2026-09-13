26e12ac570cd56b9bcc94f8e4c1d82511d30eb76
Orinbassar-Nuradil <nurdiloryrbasar@gmail.com>
2026-09-13 17:19:26 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         HttpRequestDirector director = new HttpRequestDirector();
+
         HttpRequestObjectBuilder objectBuilder = new HttpRequestObjectBuilder();
         director.makePostRequest(objectBuilder);
         HttpRequest request = objectBuilder.getResult();
         System.out.println("Object: " + request);
+
         CurlCommandBuilder curlBuilder = new CurlCommandBuilder();
         director.makePostRequest(curlBuilder);
         String curl = curlBuilder.getResult();
