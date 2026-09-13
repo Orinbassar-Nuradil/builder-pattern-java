4f4d6e5111f0a421dad6b77ec38afeaea3832f7d
Orinbassar-Nuradil <nurdiloryrbasar@gmail.com>
2026-09-13 17:16:56 +0500

    Update HttpRequest.java

--- HttpRequest.java
+++ HttpRequest.java
@@ -1,1 +1,21 @@
+public final class HttpRequest {
+    private final String method;
+    private final String url;
+    private final String body;
+
+    HttpRequest(String method, String url, String body) {
+        this.method = method;
+        this.url = url;
+        this.body = body;
+    }
+
+    public String getMethod() { return method; }
+    public String getUrl() { return url; }
+    public String getBody() { return body; }
+
+    @Override
+    public String toString() {
+        return "HttpRequest{method='" + method + "', url='" + url + "', body='" + body + "'}";
+    }
+}
 
